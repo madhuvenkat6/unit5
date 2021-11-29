a1e549d7ac66b3ec26954789e50c882c618b350c
madhuvenkat <madhuvenkatmedicharla@gmail.com>
2021-11-29 15:13:01 +0530

    contectapi

--- evalutions/coding1/bookmyshow/src/components/pages/Login.jsx
+++ evalutions/coding1/bookmyshow/src/components/pages/Login.jsx
@@ -1,5 +1,15 @@
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
+import "../Nav.css"
 export function Login() {
-    return<div>
-       Login
+    const {token} = useContext(AuthContext);
+    console.log(token);
+    return <div className="login-page">
+        <div className="login" >
+            <input type="email" placeholder="Enter email" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+        </div>
+
     </div>
 }
--- evalutions/coding1/bookmyshow/src/components/pages/Shows.jsx
+++ evalutions/coding1/bookmyshow/src/components/pages/Shows.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
+
 export function Shows() {
+    const {token} = useContext(AuthContext);
+
     return<div>
         Shows
     </div>
